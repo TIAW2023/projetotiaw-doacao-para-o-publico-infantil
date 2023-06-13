897c0b84afc84eb094ec8f5ae2da7570880d5073
NOGMarcondes <130397785+NOGMarcondes@users.noreply.github.com>
2023-06-13 10:33:24 -0300

    Update fazerpedido.js

--- codigo/JavaScript/fazerpedido.js
+++ codigo/JavaScript/fazerpedido.js
@@ -1,45 +1,46 @@
-function salvarTudo() {
-  // Salvar Nome
-  var nome = document.getElementById("nome").value;
 
-  // Salvar Endereço
-  var cep = document.getElementById("cep").value;
-  var rua = document.getElementById("rua").value;
-  var numero = document.getElementById("numero").value;
-  var bairro = document.getElementById("bairro").value;
-  var cidade = document.getElementById("cidade").value;
-  var estado = document.getElementById("estado").value;
+        // Função para salvar os dados de uma pessoa no Local Storage
+        function salvarPessoa() {
+            // Obter os valores dos campos de entrada
+            var nome = document.getElementById("nome").value;
+            var cep = document.getElementById("cep").value;
+            var rua = document.getElementById("rua").value;
+            var numero = document.getElementById("numero").value;
+            var bairro = document.getElementById("bairro").value;
+            var cidade = document.getElementById("cidade").value;
+            var estado = document.getElementById("estado").value;
+            var observacoes = document.getElementById("observacoes").value;
+            var opcoes = [];
+            
+            var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+            checkboxes.forEach(function (checkbox) {
+                opcoes.push(checkbox.value);
+            });
+            
+            // Criar um objeto com os dados da pessoa
+            var pessoa = {
+                nome: nome,
+                endereco: {
+                    cep: cep,
+                    rua: rua,
+                    numero: numero,
+                    bairro: bairro,
+                    cidade: cidade,
+                    estado: estado
+                },
+                observacoes: observacoes,
+                opcoes: opcoes
+            };
+            
+            // Verificar se já existem informações de pessoas no Local Storage
+            var pessoas = JSON.parse(localStorage.getItem("pessoas")) || [];
+            
+            // Adicionar os dados da pessoa ao array
+            pessoas.push(pessoa);
+            
+            // Salvar o array atualizado no Local Storage
+            localStorage.setItem("pessoas", JSON.stringify(pessoas));
+            
+            console.log("Dados da pessoa salvos no Local Storage.");
+        }
 
-  var endereco = {
-    cep: cep,
-    rua: rua,
-    numero: numero,
-    bairro: bairro,
-    cidade: cidade,
-    estado: estado,
-  };
-
-  // Salvar Observações
-  var observacoes = document.getElementById("observacoes").value;
-
-  // Salvar Seleção de Opções
-  var opcoes = [];
-
-  var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
-  checkboxes.forEach(function (checkbox) {
-    opcoes.push(checkbox.value);
-  });
-
-  // Criar objeto com todas as informações
-  var todasInformacoes = {
-    nome: nome,
-    endereco: endereco,
-    observacoes: observacoes,
-    opcoes: opcoes,
-  };
-
-  var jsonTodasInformacoes = JSON.stringify(todasInformacoes);
-  localStorage.setItem("todasInformacoes", jsonTodasInformacoes);
-
-  console.log("Todas as informações salvas no Local Storage.");
-}
