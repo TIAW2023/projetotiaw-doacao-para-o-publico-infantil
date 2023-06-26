7ee08b7403a38d089c8c9bb3a7f6ce64fde87cf4
Armintas-Fernandes <127865332+Armintas-Fernandes@users.noreply.github.com>
2023-06-26 12:34:26 -0300

    Update home.js

--- codigo/JavaScript/home.js
+++ codigo/JavaScript/home.js
@@ -76,4 +76,75 @@ function imprimir() {
   else { alert("Não possuí pedidos cadastrados.") }
 }
 
-onload = () => { imprimir() } 
+function mostrarprodutos() {
+  var pessoas = JSON.parse(localStorage.getItem("pessoas")); // Convertendo os valores do localstorage para Json.
+  let tela = document.getElementById('tela'); // Mostrar conteúdo na div tela do HTML.
+  var strHtml = '';
+
+  var opcoes = [];
+  // Armazenando as opções do usuário no vetor opções.
+  var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+  checkboxes.forEach(function (checkbox) {
+    opcoes.push(checkbox.value);
+  });
+
+  // Filtrar os produtos que possuem as palavras-chave fornecidas pelo usuário
+  var produtosFiltrados = pessoas.filter(function (pessoa) {
+    var possuiPalavrasChave = opcoes.every(function (opcao) {
+      return pessoa.opcoes.includes(opcao);
+    });
+    return possuiPalavrasChave;
+  });
+
+  // Gerar a string HTML para exibir os produtos filtrados
+  function quebrarLinha(texto, caracteresPorLinha) {
+    const regex = new RegExp(`.{1,${caracteresPorLinha}}`, 'g');
+    return texto.match(regex).join('\n');
+  }
+
+  produtosFiltrados.forEach(function (pessoa) {
+    const observacoesQuebradas = quebrarLinha(pessoa.observacoes, 50);
+
+    strHtml +=
+      `<div class="mx-2">
+              <div class="row fs-2 mt-2">
+                <div class="col-auto">
+                  ${pessoa.nome}
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-auto">
+                  ${pessoa.endereco.rua}, ${pessoa.endereco.numero}, ${pessoa.endereco.bairro}, ${pessoa.endereco.cidade}, ${pessoa.endereco.estado}.
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-12 mt-3">
+                  <p style="white-space: pre-line;">${observacoesQuebradas}</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-auto my-2">
+                  <b>Palavras-Chave: </b>${pessoa.opcoes}.
+                </div>
+              </div>
+              <hr class="linha">
+            </div>`;
+  });
+  tela.innerHTML = strHtml;
+}
+function verificarLogin() {
+  var logado = localStorage.getItem('logado');
+
+  if (logado != null) {
+    // Se estiver logado, ocultar os elementos de login e cadastrar
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('cadastrar').style.display = 'none';
+  }
+  else{
+  document.getElementById('meuperfil').style.display = 'none';
+  document.getElementById('fazerpedido').style.display = 'none';}
+}
+
+document.addEventListener('DOMContentLoaded', verificarLogin);
+
+onload = () => { mostrarprodutos()} 
