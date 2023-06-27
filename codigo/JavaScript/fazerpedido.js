604a87f6e5af3ec25481d7f6d2c962364a6593d4
NOGMarcondes <130397785+NOGMarcondes@users.noreply.github.com>
2023-06-26 21:45:04 -0300

    Update fazerpedido.js

--- codigo/JavaScript/fazerpedido.js
+++ codigo/JavaScript/fazerpedido.js
@@ -27,85 +27,89 @@ let efetuarLogin = (email, password) => {
   }
 };
 
-document.getElementById("login-form").addEventListener("submit", (event) => {
-  event.preventDefault();
+let salvarPessoa = () => {
+  let logadoJSON = localStorage.getItem("logado");
+  let logadoIndex = logadoJSON ? JSON.parse(logadoJSON) : -1;
 
-  let email = document.getElementById("email").value;
-  let senha = document.getElementById("password").value;
+  if (logadoIndex !== -1) {
+    let usersJSON = localStorage.getItem("users");
+    let users = usersJSON ? JSON.parse(usersJSON) : [];
 
-  efetuarLogin(email, senha);
+    let logado = users[logadoIndex];
 
-  document.getElementById("email").value = "";
-  document.getElementById("password").value = "";
-});
+    let nome = document.getElementById("nome").value;
+    let cep = document.getElementById("cep").value;
+    let rua = document.getElementById("rua").value;
+    let numero = document.getElementById("numero").value;
+    let bairro = document.getElementById("bairro").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+    let observacoes = document.getElementById("observacoes").value;
+    let opcoes = [];
 
-function salvarPessoa() {
-  // Obter o índice do usuário logado
-  var usuarioIndex = JSON.parse(localStorage.getItem("logado"));
+    let checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+    checkboxes.forEach(function (checkbox) {
+      opcoes.push(checkbox.value);
+    });
 
-  // Verificar se o usuário está logado
-  if (isNaN(usuarioIndex) || usuarioIndex < 0 || usuarioIndex >= users.length) {
-    alert("É necessário fazer o login antes de salvar os dados.");
-    return;
+    if (
+      !nome ||
+      !cep ||
+      !rua ||
+      !numero ||
+      !bairro ||
+      !cidade ||
+      !estado ||
+      opcoes.length == 0
+    ) {
+      alert("Por favor, preencha todos os campos!");
+      return;
+    }
+
+    let pessoa = {
+      nome: nome,
+      endereco: {
+        cep: cep,
+        rua: rua,
+        numero: numero,
+        bairro: bairro,
+        cidade: cidade,
+        estado: estado,
+      },
+      observacoes: observacoes,
+      opcoes: opcoes,
+    };
+
+    if (!logado.pessoas) {
+      logado.pessoas = [];
+    }
+
+    logado.pessoas.push(pessoa);
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+    document.getElementById("nome").value = "";
+    document.getElementById("cep").value = "";
+    document.getElementById("rua").value = "";
+    document.getElementById("numero").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("estado").value = "";
+    document.getElementById("observacoes").value = "";
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+
+    alert("Pedido realizado! Os dados foram salvos.");
+  } else {
+    alert("Faça o login para salvar os dados.");
   }
+};
 
-  // Obter os valores dos campos de entrada
-  var nome = document.getElementById("nome").value;
-  var cep = document.getElementById("cep").value;
-  var rua = document.getElementById("rua").value;
-  var numero = document.getElementById("numero").value;
-  var bairro = document.getElementById("bairro").value;
-  var cidade = document.getElementById("cidade").value;
-  var estado = document.getElementById("estado").value;
-  var observacoes = document.getElementById("observacoes").value;
-  var opcoes = [];
-
-  var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
-  checkboxes.forEach(function (checkbox) {
-    opcoes.push(checkbox.value);
-  });
-
-  // Criar um objeto com os dados da pessoa
-  var pessoa = {
-    userId: users[usuarioIndex].id,
-    nome: nome,
-    endereco: {
-      cep: cep,
-      rua: rua,
-      numero: numero,
-      bairro: bairro,
-      cidade: cidade,
-      estado: estado,
-    },
-    observacoes: observacoes,
-    opcoes: opcoes,
-  };
-
-  // Verificar se já existem informações de pessoas no Local Storage
-  var pessoas = JSON.parse(localStorage.getItem("pessoas")) || [];
-
-  // Adicionar os dados da pessoa ao array
-  pessoas.push(pessoa);
-
-  // Salvar o array atualizado no Local Storage
-  localStorage.setItem("pessoas", JSON.stringify(pessoas));
-
-  // Limpar os campos de entrada
-  document.getElementById("nome").value = "";
-  document.getElementById("cep").value = "";
-  document.getElementById("rua").value = "";
-  document.getElementById("numero").value = "";
-  document.getElementById("bairro").value = "";
-  document.getElementById("cidade").value = "";
-  document.getElementById("estado").value = "";
-  document.getElementById("observacoes").value = "";
-  checkboxes.forEach(function (checkbox) {
-    checkbox.checked = false;
-  });
-
-  // Exibir a mensagem de confirmação
-  alert("Pedido realizado! Os dados foram salvos.");
-}
+document.getElementById("salvar-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  salvarPessoa();
+});
 
 function validarNome() {
   var nomeInput = document.getElementById("nome");
