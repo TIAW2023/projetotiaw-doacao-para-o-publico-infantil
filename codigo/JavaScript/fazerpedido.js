a1f34190f336589df6aecc5fd188033f6ce11b09
NOGMarcondes <130397785+NOGMarcondes@users.noreply.github.com>
2023-06-26 22:53:58 -0300

    Update fazerpedido.js

--- codigo/JavaScript/fazerpedido.js
+++ codigo/JavaScript/fazerpedido.js
@@ -31,6 +31,17 @@ function salvarPessoa() {
     alert("Por favor, preencha todos os campos!");
     return; // Parar a execução da função
   }
+
+  // Verificar se o usuário está logado
+  var usuarioIndex = JSON.parse(localStorage.getItem("logado"));
+  if (usuarioIndex === null) {
+    alert("Nenhum usuário logado. Faça o login para prosseguir.");
+    return;
+  }
+
+  // Verificar se já existem informações de pessoas no Local Storage
+  var pessoas = JSON.parse(localStorage.getItem("pessoas")) || [];
+
   // Criar um objeto com os dados da pessoa
   var pessoa = {
     nome: nome,
@@ -45,9 +56,6 @@ function salvarPessoa() {
     observacoes: observacoes,
     opcoes: opcoes,
   };
-
-  // Verificar se já existem informações de pessoas no Local Storage
-  var pessoas = JSON.parse(localStorage.getItem("pessoas")) || [];
 
   // Adicionar os dados da pessoa ao array
   pessoas.push(pessoa);
@@ -108,44 +116,3 @@ function limparEndereco() {
   document.getElementById("cidade").value = "";
   document.getElementById("estado").value = "";
 }
-
-// Verificar se já existem usuários no localStorage
-let usersJSON = localStorage.getItem("users");
-let users = usersJSON ? JSON.parse(usersJSON) : [];
-
-console.log("Quantidade de usuários:", users.length);
-
-let efetuarLogin = (email, password) => {
-  let usersJSON = localStorage.getItem("users");
-  let users = usersJSON ? JSON.parse(usersJSON) : [];
-
-  let usuario = null;
-  let usuarioIndex = -1;
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].email === email) {
-      usuario = users[i];
-      usuarioIndex = i;
-      break;
-    }
-  }
-
-  if (usuario && usuario.password && usuario.password === password) {
-    localStorage.setItem("logado", JSON.stringify(usuarioIndex));
-    alert("Login realizado com sucesso!");
-    window.location.href = "home.html";
-  } else {
-    alert("Email ou senha incorretos. Por favor, tente novamente.");
-  }
-};
-
-document.getElementById("login-form").addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let email = document.getElementById("email").value;
-  let senha = document.getElementById("password").value;
-
-  efetuarLogin(email, senha);
-
-  document.getElementById("email").value = "";
-  document.getElementById("password").value = "";
-});
